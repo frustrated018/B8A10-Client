72968eb5eacde3a28b2bd08d80c6896a49d91c01
frustrated018 <niloyd018@gmail.com>
2023-10-18 03:29:22 +0600

    Set up the nested routes for the page and everthing seems to be working

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <div className="text-center text-4xl text-red-500 mt-10">This is the home page</div>
+        </div>
+    );
+};
+
+export default Home;
