ab170dc437131fd6bd3327ea7f9f3ea14557e761
frustrated018 <niloyd018@gmail.com>
2023-10-18 14:11:20 +0600

    Added waves between the banner and content section in the home page

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className="h-[50vh] w-full bg-[#007b7d]">
+      <div className="text-center text-4xl text-blue-500 pt-10">
+        This is the Banner section
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,28 @@
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="text-center text-4xl text-red-500 mt-10">This is the home page</div>
+  return (
+    <div>
+      <div>
+        <Banner></Banner>
+        {/* Empty div for the wave */}
+        <div
+          style={{
+            aspectRatio: "960/200",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundImage: "url(src/assets/wave1.svg)",
+          }}
+        ></div>
+        <div className="h-[100vh] w-full bg-[#F5F5F5]">
+          <div className="text-center text-4xl text-yellow-500 pt-10">
+            This is the Card section
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
