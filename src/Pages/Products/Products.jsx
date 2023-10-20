4e742b12e61689f61d19a9e519e9049b396201eb
frustrated018 <niloyd018@gmail.com>
2023-10-20 14:28:26 +0600

    Made a basic card

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,14 +1,19 @@
 import { useLoaderData } from "react-router-dom";
+import ProductCard from "../../Components/ProductCard/ProductCard";
 
 const Products = () => {
 
     const products = useLoaderData();
-    console.log(products);
 
 
     return (
         <div className="bg-[#F5F5F5]">
             <div className="text-center text-4xl text-red-500 pt-20"> all the product cards will be here</div>
+            <div className="grid grid-cols-2 gap-3">
+                {
+                    products.map(product => <ProductCard key={product._id} product={product}></ProductCard>)
+                }
+            </div>
         </div>
     );
 };
