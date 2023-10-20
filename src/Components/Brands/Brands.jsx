6f8ca5fba873da77d2a773726de35c021e939991
frustrated018 <niloyd018@gmail.com>
2023-10-20 13:05:59 +0600

    fixed a bit of padding issue

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -7,7 +7,7 @@ const Brands = ({brands}) => {
 
   return (
     <div>
-      <div className="text-red-500 text-4xl text-center ">
+      <div className="text-red-500 text-4xl text-center pb-10">
         Find Products Based on Brands
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mx-10 md:mx-10 mx-5">
