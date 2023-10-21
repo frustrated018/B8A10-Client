a56456f34d3c53787597bc6c4bebfb167b8b8c6d
frustrated018 <niloyd018@gmail.com>
2023-10-21 21:24:21 +0600

    Changed the banner design in the home page

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,92 +3,36 @@ import "aos/dist/aos.css";
 AOS.init();
 const Banner = () => {
   return (
-    <div className="bg-[#007B7D] text-white relative flex items-center">
-      <div
-        className="max-w-screen-xl mx-auto px-6 w-full"
-        data-aos="fade-right"
-        data-aos-delay="400"
-      >
-        <section className="mt-10">
-          <div
-            className="relative flex items-center h-full rounded-lg"
-            style={{
-              background: `url(https://i.ibb.co/pbwM737/banner3.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-gray-400 opacity-25 rounded-lg"></div>
-            <div className="py-10 text-left w-full lg:w-[80%] lg:h-[30%]">
-              <div className="w-3/4 max-w-2xl mx-auto text-gray-900 relative" data-aos="fade-left"
-                data-aos-delay="200">
-                <h1 className="text-4xl font-extrabold mb-4">
-                  Discover the Latest Trends
-                </h1>
-                <p className="text-lg">
-                  Explore our collection of the trendiest fashion and apparel,
-                  carefully curated just for you.
-                </p>
-              </div>
+    <div>
+      <section className="relative bg-[url(https://i.ibb.co/pbwM737/banner3.jpg)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-[#007b7d] opacity-25 rounded-lg"></div>
+
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
+              Discover the
+              <strong className="block font-extrabold text-[#00a8a9]">
+                Latest Trends
+              </strong>
+            </h1>
+
+            <p className="mt-4 text-white max-w-lg sm:text-xl/relaxed">
+              Explore our collection of the trendiest fashion and apparel,
+              carefully curated just for you.
+            </p>
+
+            <div className="mt-8 flex flex-wrap gap-4 text-center">
+              <button className="block w-full rounded bg-[#007b7d] px-12 py-3 text-base font-medium text-white shadow hover:bg-[#00a8a9] focus:outline-none focus:ring active:bg-[#00a8a9] sm:w-auto">
+                Get Started
+              </button>
+
+              <button className="block w-full rounded bg-[#f5f5f5] px-12 py-3 text-base font-medium text-[#007b7d] shadow  focus:outline-none focus:ring sm:w-auto">
+                Learn More
+              </button>
             </div>
           </div>
-        </section>
-        <section className="mt-10">
-          <div
-            className="relative flex items-center h-full rounded-lg"
-            style={{
-              background: `url(https://i.ibb.co/drHkKWd/banner1.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-gray-400 opacity-25  rounded-lg"></div>
-            <div className="py-10 text-left w-full lg:w-[80%] lg:h-[30%]">
-              <div
-                className="w-3/4 max-w-2xl mx-auto text-gray-900 relative"
-                data-aos="fade-left"
-                data-aos-delay="200"
-              >
-                <h1 className="text-4xl font-extrabold mb-4">
-                  Elevate Your Style
-                </h1>
-                <p className="text-lg">
-                  Upgrade your wardrobe with our exclusive selection of elegant
-                  clothing and accessories.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="mt-10">
-          <div
-            className="relative flex items-center h-full rounded-lg"
-            style={{
-              background: `url(https://i.ibb.co/cbBNqjP/banner4.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-gray-400 opacity-25  rounded-lg"></div>
-            <div className="py-10 text-left w-full lg:w-[80%] lg.h-[30%]">
-              <div className="w-3/4 max-w-2xl mx-auto text-gray-900 relative" data-aos="fade-left"
-                data-aos-delay="200">
-                <h1 className="text-4xl font-extrabold mb-4">
-                  Unleash Your Unique Style
-                </h1>
-                <p className="text-lg">
-                  Embrace your individuality with our diverse range of fashion
-                  items, designed to express your personality.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 };
