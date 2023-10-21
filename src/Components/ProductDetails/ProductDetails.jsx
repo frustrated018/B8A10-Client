123cc72f12a299574c277fa499ea937897a9aea3
frustrated018 <niloyd018@gmail.com>
2023-10-21 20:56:02 +0600

    Edited the product details page a little bit

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -10,6 +10,7 @@ const ProductDetails = () => {
 
   const {
     longerDescription,
+    type,
     productName,
     photo,
     price,
@@ -33,13 +34,16 @@ const ProductDetails = () => {
 
   //   Adding to cart
   const handleAddToCart = () => {
-    fetch("https://clothing-cove-server-jxbnyope8-niloys-projects-59c08af4.vercel.app/cart", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(newProduct),
-    })
+    fetch(
+      "https://clothing-cove-server-jxbnyope8-niloys-projects-59c08af4.vercel.app/cart",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(newProduct),
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -52,18 +56,49 @@ const ProductDetails = () => {
   };
 
   return (
-    <div className="flex justify-center items-center lg:mt-20 md:mt-10 mt-5">
-      <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm w-[80%] md:w-1/2">
+    <div className="flex justify-center items-center lg:pt-20 md:pt-10 py-5"
+    style={{
+      backgroundImage: "url(https://i.ibb.co/gMW1ZLX/Contour-Line.png)",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      position: "relative",
+    }}
+    >
+      <article className="overflow-hidden rounded-lg border border-gray-100 bg-[#f5f5f5] shadow-sm w-[80%] md:w-1/2">
         <img alt="Office" src={photo} className="h-56 w-full object-cover" />
 
         <div className="p-4 sm:p-6">
-          <a href="#">
-            <h3 className="text-lg font-medium text-gray-900">{productName}</h3>
-          </a>
+          {/* Longer description */}
+          <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                <dt className="font-medium text-gray-900">Brand</dt>
+                <dd className="text-gray-700 sm:col-span-2">{brandName}</dd>
+              </div>
 
-          <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-            {longerDescription}
-          </p>
+              <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                <dt className="font-medium text-gray-900">Product Name</dt>
+                <dd className="text-gray-700 sm:col-span-2">{productName}</dd>
+              </div>
+
+              <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                <dt className="font-medium text-gray-900">Type</dt>
+                <dd className="text-gray-700 sm:col-span-2">{type}</dd>
+              </div>
+
+              <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                <dt className="font-medium text-gray-900">Price</dt>
+                <dd className="text-gray-700 sm:col-span-2">${price}</dd>
+              </div>
+
+              <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                <dt className="font-medium text-gray-900">Description</dt>
+                <dd className="text-gray-700 sm:col-span-2">
+                 {longerDescription}
+                </dd>
+              </div>
+            </dl>
+          </div>
 
           <button
             onClick={handleAddToCart}
